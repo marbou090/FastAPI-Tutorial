99adb8ab740103d0ef884eb38b77dae94bf246e2
marubou090 <rikka2217@gmail.com>
2021-08-12 23:08:38 +0900

    必須クエリパラメータ

--- query.py
+++ query.py
@@ -1,35 +1,12 @@
-from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: Optional[str]= None, short: bool= False
-    ):
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
     """
-    関数パラメータ q はオプショナルとなり、デフォルトでは None 
-    この場合、以下にアクセスすると:
-    http://127.0.0.1:8000/items/foo?short=1
-    もしくは、
-    http://127.0.0.1:8000/items/foo?short=True
-    もしくは、
-    http://127.0.0.1:8000/items/foo?short=true
-    もしくは、
-    http://127.0.0.1:8000/items/foo?short=on
-    もしくは、
-    http://127.0.0.1:8000/items/foo?short=yes
-
-    http://127.0.0.1:8000/items/maru?short=1&q=hoge&short=False
-    だと
-    {"item_id":"maru","q":"hoge","description":"This is an amazing item that has a long description"}
+    needyは必須のクエリパラメータになる
     """
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q":q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+    item = {"item_id": item_id, "needy": needy}
     return item
