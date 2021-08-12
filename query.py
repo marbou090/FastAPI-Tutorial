f5e44f3788833e00393835ba1b7f3754fc57e04e
marubou090 <rikka2217@gmail.com>
2021-08-12 22:53:07 +0900

    オプショナルなパラメータ

--- query.py
+++ query.py
@@ -4,12 +4,11 @@ app = FastAPI()
 
 fake_items_db  = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str]= None):
     """
-    クエリパラメータは固定パスではない。デフォルト値を持つだけ。
-    http://127.0.0.1:8000/items/?skip=20　にアクセスすると関数内のパラメータは
-    skip=20,limit=10になる。ふむ。
+    関数パラメータ q はオプショナルとなり、デフォルトでは None 
     """
-    return fake_items_db[skip : skip + limit]
-
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
