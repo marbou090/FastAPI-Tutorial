de8d66468e191f9674c4cc91393ce8bacfdb1c4d
marubou090 <rikka2217@gmail.com>
2021-08-12 23:21:34 +0900

    データモデル

---
+++ requestbody.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str]= None
+    price: float
+    tax: Optional[float]= None
+
+app = FastAPI()
+
+@app.get("/items/")
+async def create_item(item: Item):
+    return item
