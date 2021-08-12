1eecc8ee266fef2bfc1d946ab8f3fd7a43f71080
marubou090 <rikka2217@gmail.com>
2021-08-12 21:40:02 +0900

    固定path

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/users/me")
+async def read_item_me():
+    return {"user_id": "the current user"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/{user_id}")
+async def read_item(user_id: str):
+    return {"user_id": user_id}
